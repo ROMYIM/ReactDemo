3dcba28470e9ead2b99e857fa24bfcc6e99d2c63
ROMYIM <1500975523@qq.com>
2019-07-05 18:17:55 +0800

    雨滴实体类

--- ClientApp/src/entities/Raindrop.js
+++ ClientApp/src/entities/Raindrop.js
@@ -11,12 +11,22 @@ export default class Raindrop {
         // 雨滴颜色
         this.color = props.color;
 
-        // 初始化雨滴的位置
+        // 雨滴透明度
+        this.transparency = 0.8;
+
+        // 雨滴的位置
         this.positionX = randomFloor(this.canvasWidth);
         this.positionY = random(this.canvasHeight * 0.8, this.canvasHeight * 0.9);
 
+        // 雨滴下落的高度
+        this.height = this.positionY;
+
+        // 雨滴下落方向
+        this.direction = 0;
+
         // 雨滴下落速度
         this.speed = random(8, 9);
+
     }
     
 }
